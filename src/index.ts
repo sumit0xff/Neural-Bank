52fbd53f5d225c5472054f1eb20afbf4296481df
sumit0xff <216392422+sumit0xff@users.noreply.github.com>
2026-09-23 22:39:02 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
+import ast = require("typescript/unstable/ast");
+
+
+const app = express();
+app.use(express.json());
+
+app.post("/api/v1/signup",(req, res)=>{
+   
+})
+app.post("/api/v1/signin",(req,res)=>{
+
+})
+
+app.get("/api/v1/content",(req,res)=>{
+    
+
+})
+app.post("/api/v1/content",(req,res)=>{
+
+})
+app.delete("/api/v1/content",(req,res)=>
+{
+
+})
+app.post("/api/v1/neural/share",(req,res)=>{
+
+})
+app.get("/api/v1/neural/sharelink",(req,res)=>{
+
+})
